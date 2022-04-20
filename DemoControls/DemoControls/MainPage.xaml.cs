8b88682b74eab3bf3d00d3a296976d3ba78a1cc9
Hugo Felipe Torrico Marquez <htorrico@tecsup.edu.pe>
2022-04-20 11:57:27 -0500

    Add project files.

---
+++ DemoControls/DemoControls/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DemoControls
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            dtpFecha.Date = DateTime.Today;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Titulo", "Mensaje", "Ok");
+        }
+    }
+}
